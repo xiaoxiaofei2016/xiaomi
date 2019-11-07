199712ed2a5cfd861ce316a2228b86c19c407c2e
dufajun <2695647975@qq.com>
2019-11-07 10:26:55 +0800

    小米商城

--- src/components/mainPage/flash-purchase.vue
+++ src/components/mainPage/flash-purchase.vue
@@ -6,8 +6,8 @@
     <div class="box-bd clearfix">
       <div class="row">
         <div class="flashsale-countdown rainbow-item-4 span4">
-          <div class="round">10:00 场</div>
-          <img src="" alt="">
+          <div class="round">{{resttime}} 场</div>
+          <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573102436911&di=b8a0884f46bf82236984fbf77058efe4&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201708%2F19%2F005327j9q0ya9u8aqfv9al.png" alt="" width="34" height="53">
           <div class="desc">距离结束还有</div>
           <div class="countdown clearfix">
             <span id="h">{{hour}}</span>
@@ -18,7 +18,24 @@
           </div>
         </div>
         <div class="span16 flashsale-list swiper-container swiper-no-swiping">
-           <!-- 轮播  -->
+          <ul class="swiper-wrapper">
+            <li class="swiper-slide rainbow-item-1">
+              <router-link to="">
+                <div class="content">
+                  <div class="thumb">
+                    <img src="" alt="" width="160" height="160">
+                  </div>
+                  <h3 class="title"></h3>
+                  <p class="desc"></p>
+                  <p class="price">
+                    <span></span>
+                    <del></del>
+                  </p>
+                  <span class="flag"></span>
+                </div>
+              </router-link>
+            </li>
+          </ul>
         </div>
         <div class="swiper-controls">
           <span class="swiper-flashsale-prev">
@@ -34,14 +51,24 @@
 </template>
 
 <script>
-// import '@/utils/flash-purchase.js'
+import 'swiper/dist/css/swiper.css'
+import Swiper from 'swiper'
 export default {
   data () {
     return {
-      hour: '0',
-      minute: '0',
-      second: '0',
-      start: '2019-11-10 0:0:0'
+      hour: '',
+      minute: '',
+      second: '',
+      start: '2019-11-9 09:52:00'
+    }
+  },
+  computed: {
+    resttime () {
+      let resttime1 = this.start
+      let a = resttime1.substr(resttime1.indexOf(' ') + 1, 2)
+      let b = resttime1.substr(resttime1.indexOf(':') + 1, 2)
+      let resttime = `${a}:${b}`
+      return resttime
     }
   },
   methods: {
@@ -49,9 +76,12 @@ export default {
       var newDate = new Date()
       var newDate1 = new Date(this.start)
       var restTime = newDate1.getTime() - newDate.getTime() // 计算剩余的毫秒数
+      // console.log(restTime)
+      if (restTime <= 0) restTime === 0
       var hours = parseInt(restTime / 1000 / 60 / 60 % 24, 10) // 计算剩余的小时
       var minutes = parseInt(restTime / 1000 / 60 % 60, 10)// 计算剩余的分钟
       var seconds = parseInt(restTime / 1000 % 60, 10)// 计算剩余的秒数
+      
       hours = this.fix(hours, 2)
       minutes = this.fix(minutes, 2)
       seconds = this.fix(seconds, 2)
@@ -64,9 +94,12 @@ export default {
     }
   },
   created () {
-    setInterval(() => {
+    let time = setInterval(() => {
       this.leftTimer()
     }, 1000)
+  },
+  destroyed () {
+    clearInterval(time)
   }
 }
 </script>
